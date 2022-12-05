2fa4e5534c916037ebab746e9491b9a4a8dd581b
Krupa Bhojawala <krupa.bhojawala@wissen.com>
2022-12-05 17:44:58 +0530

    Client Name serch and location check validation added

--- src/components/client-form/ClientForm.js
+++ src/components/client-form/ClientForm.js
@@ -5,7 +5,9 @@ import {
     DialogActions,
     Dialog,
     Button,
-    TextField
+    TextField,
+    InputBase,
+    Autocomplete
 } from '@mui/material';
 import { Row, Col, Container } from "react-bootstrap";
 
@@ -13,38 +15,56 @@ const ClientForm = ({ open, handleClose }) => {
     const [clientName, setClientName] = useState('');
     const [clientLocation, setClientLocation] = useState('');
     const [addClientValidation, setAddClientValidation] = useState(false);
+    const [clientSearch, setClientSearch] = useState([]);
     const url = "/api/client";
 
     useEffect(() => {
-        if(clientName.length >= 3 && clientLocation.length >= 3)
+        if (clientName.length >= 3 && clientLocation.length >= 3)
             setAddClientValidation(true);
         else
             setAddClientValidation(false);
-    },[clientLocation, clientName]);
+
+        if (clientSearch[0]?.clientName === clientName) {
+            let flag = false;
+            clientSearch.forEach((client) => {
+                if (client.clientLocation === clientLocation) {
+                    flag = true;
+                }
+            });
+            setAddClientValidation(!flag);
+        }
+    }, [clientLocation, clientName]);
+
+    useEffect(() => {
+        fetch('/api/client/search?clientName=' + clientName)
+            .then((resp) => resp.json())
+            .then((resp) => setClientSearch(resp));
+        console.log(clientSearch[0]?.clientName === clientName);
+    }, [clientName]);
 
     const saveClient = () => {
         const data = [{
             "clientLocation": clientLocation,
             "clientName": clientName
-          }];
+        }];
         fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(data)
-          })
-          .then((resp) => console.log(resp));
-          setClientLocation('');
-          setClientName('');
-          handleClose();
-      };
-
-      const onClose = () => {
+        })
+            .then((resp) => console.log(resp));
         setClientLocation('');
         setClientName('');
         handleClose();
-      };
+    };
+
+    const onClose = () => {
+        setClientLocation('');
+        setClientName('');
+        handleClose();
+    };
 
     return (
         <React.Fragment>
@@ -60,36 +80,55 @@ const ClientForm = ({ open, handleClose }) => {
                     <Container maxWidth="sm">
                         <Row>
                             <Col>
-                            <TextField
-                                autoFocus
-                                margin="dense"
-                                id="name"
-                                label="Client Name"
-                                type="text"
-                                fullWidth
-                                variant="standard"
-                                error={clientName.length >= 3? false : true}
-                                helperText="Enter Client's Name"
-                                required={true}
-                                value={clientName}
-                                onChange={(e) => {setClientName(e.target.value)}}
-                            />
+                                <Autocomplete
+                                    id="free-solo-demo"
+                                    freeSolo
+                                    fullWidth
+                                    options={clientSearch.map((option) => option.clientName)}
+                                    onChange={(event, newValue) => {
+                                        if (typeof newValue === 'string') {
+                                            setClientName(newValue);
+                                        } else if (newValue && newValue.inputValue) {
+                                            setClientName(newValue.inputValue);
+                                        } else {
+                                            setClientName(newValue);
+                                        }
+                                    }}
+                                    renderInput={
+                                        (params) =>
+                                            <TextField
+                                                {...params}
+                                                autoFocus
+                                                margin="dense"
+                                                id="name"
+                                                label="Client Name"
+                                                type="serch"
+                                                fullWidth
+                                                variant="standard"
+                                                error={clientName.length >= 3 ? false : true}
+                                                helperText="Enter Client's Name"
+                                                required={true}
+                                                value={clientName}
+                                                onChange={(e) => { setClientName(e.target.value) }}
+                                            />
+                                    }
+                                />
                             </Col>
                             <Col>
-                            <TextField
-                                autoFocus
-                                margin="dense"
-                                id="name"
-                                label="Client Location"
-                                type="text"
-                                fullWidth
-                                variant="standard"
-                                error={clientLocation.length >=3? false : true}
-                                helperText="Enter Client's Location."
-                                required={true}
-                                value={clientLocation}
-                                onChange={(e) => {setClientLocation(e.target.value)}}
-                            />
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="name"
+                                    label="Client Location"
+                                    type="text"
+                                    fullWidth
+                                    variant="standard"
+                                    error={clientLocation.length >= 3 ? false : true}
+                                    helperText="Enter Client's Location."
+                                    required={true}
+                                    value={clientLocation}
+                                    onChange={(e) => { setClientLocation(e.target.value) }}
+                                />
                             </Col>
                         </Row>
                     </Container>
