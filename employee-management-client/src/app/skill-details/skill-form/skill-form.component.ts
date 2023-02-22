0ef0d2240baecdcd8e65073a7cf56284f16cc2a7
saad95544 <mohammad.saad@wissen.com>
2023-02-22 06:16:45 +0000

    showing data in skill grid after create or edit. And alerting user if skill already exists.

--- employee-management-client/src/app/skill-details/skill-form/skill-form.component.ts
+++ employee-management-client/src/app/skill-details/skill-form/skill-form.component.ts
@@ -1,19 +1,21 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { ISkill } from '../ISkill';
 import { SkillService } from '../skill.service';
 
 @Component({
   selector: 'app-skill-form',
   templateUrl: './skill-form.component.html',
-  styleUrls: ['./skill-form.component.scss']
+  styleUrls: ['./skill-form.component.scss'],
 })
 export class SkillFormComponent implements OnInit {
+  constructor(
+    public service: SkillService,
+    public dialogRef: MatDialogRef<SkillFormComponent>
+  ) {}
 
-  constructor(public service: SkillService,
-    public dialogRef: MatDialogRef<SkillFormComponent>) { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onClear() {
     this.service.form.reset();
@@ -21,13 +23,25 @@ export class SkillFormComponent implements OnInit {
   }
 
   onSubmit() {
+    let found: boolean;
     if (this.service.form.valid) {
-      console.log(this.service.form.value)
-      this.service.insertSkill(this.service.form.value).subscribe(data => {
-
-        console.log(data);
-
-      });
+      console.log(this.service.form.value);
+      this.service.insertSkill(this.service.form.value).subscribe(
+        (data) => {
+          this.service.skillList.forEach((skill, index) => {
+            if (Array.isArray(data) && skill.skillId === data[0].skillId) {
+              this.service.skillList[index] = data[0];
+              this.service.skillList = [...this.service.skillList];
+              found = true;
+            }
+          });
+          if (!found && Array.isArray(data))
+            this.service.skillList = [...this.service.skillList, data[0]];
+        },
+        (err) => {
+          if (err.status === 400) alert(err.error[0].errorMessage);
+        }
+      );
       this.service.form.reset();
       this.service.initializeFormGroup();
       this.onClose();
@@ -39,5 +53,4 @@ export class SkillFormComponent implements OnInit {
     this.service.initializeFormGroup();
     this.dialogRef.close();
   }
-
 }
