360ffefb47fa2cbd332de69be33e0173db92a72c
Shreyans Gosalia <shreyansgosalia18@gmail.com>
2023-02-23 20:23:39 +0530

    Navbar Code changes with routing enabled

--- employee-management-client/src/app/navbar/navbar.component.ts
+++ employee-management-client/src/app/navbar/navbar.component.ts
@@ -1,17 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
+import { delay, filter } from 'rxjs/operators';
+import { NavigationEnd, Router } from '@angular/router';
+//import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  open: boolean = true;
-  constructor() { }
+  @ViewChild(MatSidenav, { static: true })
+  sidenav!: MatSidenav;
 
-  ngOnInit(): void {
-  }
-  toggleOpenNav() {
-    this.open = !this.open;
+  constructor(private observer: BreakpointObserver, private router: Router) {}
+
+  ngOnInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+
+    this.router.events
+      .pipe(
+        delay(0),
+        filter((e) => e instanceof NavigationEnd)
+      )
+      .subscribe(() => {
+        if (this.sidenav.mode === 'over') {
+          this.sidenav.close();
+        }
+      });
   }
 }
