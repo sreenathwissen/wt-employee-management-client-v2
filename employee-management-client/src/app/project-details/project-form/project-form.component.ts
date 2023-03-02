1590cb988d6a3e8fef35b29650ba7ea390fc68a8
ShreyansWissen <shreyans.gosalia@wissen.com>
2023-03-03 03:00:10 +0530

    toaster and error handling on service of skill, project and client

--- employee-management-client/src/app/project-details/project-form/project-form.component.ts
+++ employee-management-client/src/app/project-details/project-form/project-form.component.ts
@@ -4,30 +4,31 @@ import { ClientService } from 'src/app/client-details/client.service';
 import { IClient } from 'src/app/client-details/IClient';
 import { IProject } from '../IProject';
 import { ProjectService } from '../project.service';
+import { NotificationService } from '../../notification-service/notification.service';
 
 @Component({
   selector: 'app-project-form',
   templateUrl: './project-form.component.html',
-  styleUrls: ['./project-form.component.scss']
+  styleUrls: ['./project-form.component.scss'],
 })
 export class ProjectFormComponent implements OnInit {
   clientList!: IClient[];
 
-  constructor(public service: ProjectService,
+  constructor(
+    public service: ProjectService,
+    public notificationService: NotificationService,
     public dialogRef: MatDialogRef<ProjectFormComponent>,
-    public clientService: ClientService) { }
+    public clientService: ClientService
+  ) {}
 
   ngOnInit(): void {
-
     this.clientService.getClientData().subscribe((data: IClient[]) => {
       this.clientList = data;
-    }
-    )
+    });
   }
 
   changeClient(e: any) {
     console.log(e.target.value);
-
   }
   onClear() {
     this.service.form.reset();
@@ -37,7 +38,7 @@ export class ProjectFormComponent implements OnInit {
   onSubmit() {
     let found: boolean;
     if (this.service.form.valid) {
-      console.log(this.service.form.value)
+      console.log(this.service.form.value);
       const saveData: IProject = {
         projectId: this.service.form.value.projectId,
         projectName: this.service.form.value.projectName,
@@ -45,29 +46,44 @@ export class ProjectFormComponent implements OnInit {
         projectLead: this.service.form.value.projectLead,
         projectType: this.service.form.value.projectType,
         clientId: this.service.form.value.client.clientId,
-
       };
-      this.service.insertProject(saveData).subscribe(data => {
-
-        console.log(data);
-        this.service.projectList.forEach((project, index) => {
-          if (Array.isArray(data) && project.projectId === data[0].projectId) {
-            this.service.projectList[index] = data[0];
-            this.service.projectList = [...this.service.projectList];
-            found = true;
+      this.service.insertProject(saveData).subscribe(
+        (data) => {
+          console.log(data);
+          this.service.projectList.forEach((project, index) => {
+            if (
+              Array.isArray(data) &&
+              project.projectId === data[0].projectId
+            ) {
+              this.service.projectList[index] = data[0];
+              this.service.projectList = [...this.service.projectList];
+              found = true;
+              this.notificationService.showSuccess(
+                'Success',
+                'Project Updated Successfully'
+              );
+              this.dialogRef.close(true);
+            }
+          });
+          if (!found && Array.isArray(data)) {
+            this.notificationService.showSuccess(
+              'Success',
+              'Project Added Successfully'
+            );
+            this.service.projectList = [...this.service.projectList, data[0]];
+            this.dialogRef.close(true);
           }
-        });
-        if (!found && Array.isArray(data))
-          this.service.projectList = [...this.service.projectList, data[0]];
-
-      },
-      (err) => {
-        if (err.status === 400) alert(err.error[0].errorMessage);
-      }
+        },
+        (err) => {
+          if (err.status === 400) {
+            this.notificationService.showError(
+              'Failure',
+              'Project Already Present'
+            );
+            this.dialogRef.close(false);
+          }
+        }
       );
-      this.service.form.reset();
-      this.service.initializeFormGroup();
-      this.onClose();
     }
   }
 
@@ -76,5 +92,4 @@ export class ProjectFormComponent implements OnInit {
     this.service.initializeFormGroup();
     this.dialogRef.close();
   }
-
 }
--- employee-management-client/src/app/project-details/project.service.ts
+++ employee-management-client/src/app/project-details/project.service.ts
@@ -1,23 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
 import { IClient } from '../client-details/IClient';
 import { IProject } from '../project-details/IProject';
-
+import { ISkill } from '../skill-details/ISkill';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProjectService {
-
+  private projectUrl = 'http://localhost:8080/api/project';
   projectList!: IProject[];
 
   defaultClient: IClient = {
     clientId: 0,
     clientLocation: '',
-    clientName: ''
+    clientName: '',
   };
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {}
 
   form: FormGroup = new FormGroup({
     projectId: new FormControl(0, Validators.required),
@@ -25,7 +26,7 @@ export class ProjectService {
     projectLocation: new FormControl('', Validators.required),
     projectLead: new FormControl('', Validators.required),
     projectType: new FormControl('', Validators.required),
-    client: new FormControl(this.defaultClient, Validators.required)
+    client: new FormControl(this.defaultClient, Validators.required),
   });
 
   initializeFormGroup() {
@@ -35,34 +36,53 @@ export class ProjectService {
       projectLocation: '',
       projectLead: '',
       projectType: '',
-      client: this.defaultClient
+      client: this.defaultClient,
     });
   }
 
   getProjectData(): Observable<IProject[]> {
-    return this._http.get<IProject[]>('http://localhost:8080/api/project/allProjects');
-
+    return this._http.get<IProject[]>(this.projectUrl + '/allProjects').pipe(
+      map((res: IProject[]) => {
+        return res;
+      }),
+      catchError((err: HttpErrorResponse) => {
+        return throwError(err);
+      })
+    );
   }
 
   searchProjectData(searchPattern: String): Observable<IProject[]> {
-    return this._http.get<IProject[]>('http://localhost:8080/api/project/search?projectName=' + searchPattern);
-
+    return this._http
+      .get<IProject[]>(this.projectUrl + '/search?projectName=' + searchPattern)
+      .pipe(
+        map((res: IProject[]) => {
+          return res;
+        }),
+        catchError((err: HttpErrorResponse) => {
+          return throwError(err);
+        })
+      );
   }
 
   insertProject(project: IProject) {
     console.log(project);
-    let projectArray = []
+    let projectArray = [];
     projectArray.push(project);
     console.log(projectArray);
-
-    const headers = { 'content-type': 'application/json' }
+    const headers = { 'content-type': 'application/json' };
     const body = JSON.stringify(projectArray);
-    console.log(body)
-    return this._http.post("http://localhost:8080/api/project", body, { 'headers': headers })
+    console.log(body);
+    return this._http.post(this.projectUrl, body, { headers: headers }).pipe(
+      map((res: any) => {
+        return res;
+      }),
+      catchError((err: HttpErrorResponse) => {
+        return throwError(err);
+      })
+    );
   }
 
   populateForm(project: IProject) {
     this.form.setValue(project);
   }
-
 }
